48259d2b207fb4f144c6b3bb7ee2e4a1c47e8664
Bryce Hill <bryce@brycehill.net>
2013-08-04 14:04:22 -0700

    Add Ember template

---
+++ ember/root/app.js
@@ -0,0 +1,8 @@
+/*
+ * This would be the main js file?
+ *
+ * Stuff in {%  %} will be replaced with the data in the props object.
+ */
+ //window.{%  %}   figure out how to replace App with the name here...
+ // Global App object
+window.App = Ember.Application.create();
